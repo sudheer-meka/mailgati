4a2c4908bb593c6a2618539173ed691a2055db0a
Sudheer Meka <sudheerm16@gmail.com>
2016-06-07 22:42:34 +0530

    devise added

--- app/controllers/email_templates_controller.rb
+++ app/controllers/email_templates_controller.rb
@@ -12,6 +12,19 @@ class EmailTemplatesController < ApplicationController
   # GET /email_templates/1
   # GET /email_templates/1.json
   def show
+    @email_activities = @email_template.email_activities
+    total_processed = @email_activities.count.to_f
+    open_count = @email_activities.where(status: %w[open clicked unsubscribe spam_complaint]).count
+    delivery_count = open_count + @email_activities.where(status: 'delivered').count
+    bounce_count = @email_activities.where(status: 'bounced').count
+    clicked_count = @email_activities.where(status: 'clicked').count
+
+    if total_processed > 0
+      @stats = {total_processed: total_processed,open_count: ((open_count/total_processed)),delivery_count: ((delivery_count/total_processed)*100),clicked_count: ((clicked_count/total_processed)*100),bounce_count: ((bounce_count/total_processed)*100)}
+    else
+      @stats = {total_processed: total_processed,open_count: 0,delivery_count: 0,clicked_count: 0,bounce_count: 0}
+    end
+
   end
 
   # GET /email_templates/new

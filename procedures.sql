1225be6769708b2f20a5e2d75c42f0b3ebe1de39
Sudheer Meka <sudheerm16@gmail.com>
2016-08-13 18:00:20 +0530

    font size incresed

--- procedures.sql
+++ procedures.sql
@@ -4,7 +4,6 @@ DELIMITER $$
 CREATE PROCEDURE `new_procedure`(custom_field_ids varchar(500),group_ids varchar(500))
 BEGIN
 DECLARE ids varchar(50);
-set ids = '3,4,5';
 SELECT  GROUP_CONCAT(DISTINCT
         CONCAT('MAX(CASE WHEN custom_field_id = ''',
                custom_field_id,
@@ -17,7 +16,7 @@ FROM custom_field_values where FIND_IN_SET(custom_field_id,custom_field_ids);
 SET @sql = CONCAT('SELECT   a.name AS name,a.email as email,a.id as subscriber_id,a.subscriber_group_id as subscriber_group_id, ', @sql, '
                     FROM    subscribers a
                             LEFT JOIN custom_field_values b
-                                ON a.id = b.subscriber_id where FIND_IN_SET(a.subscriber_group_id,"',group_ids,'")
+                                ON a.id = b.subscriber_id where FIND_IN_SET(a.subscriber_group_id,"',group_ids,'") and is_active=1
                     GROUP   BY a.email');
 PREPARE stmt FROM @sql;
 EXECUTE stmt;
@@ -28,6 +27,6 @@ DELIMITER ;
 DELIMITER $$
 CREATE  PROCEDURE `get_subscribers`(in group_ids varchar(50))
 BEGIN
-select name,email,id as subscriber_id,subscriber_group_id from subscribers where FIND_IN_SET(subscriber_group_id,group_ids);
+select name,email,id as subscriber_id,subscriber_group_id from subscribers where FIND_IN_SET(subscriber_group_id,group_ids) and is_active=1;
 END$$
 DELIMITER ;
